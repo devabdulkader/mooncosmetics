9866a591aa683af91551f88453aed7f866b21718
Md. Abdul Kader <abdulemail789@gmail.com>
2024-08-07 02:17:17 +0600

    fixed social link

--- components/shared/header/SocialIcons.tsx
+++ components/shared/header/SocialIcons.tsx
@@ -3,8 +3,8 @@ import {
   FaTwitter,
   FaLinkedinIn,
   FaInstagram,
-  FaGithub,
 } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const socialIcons = [
   {
@@ -13,6 +13,7 @@ const socialIcons = [
     bgColor: "bg-blue-600",
     textColor: "text-blue-600",
     hoverBgColor: "hover:bg-blue-600",
+    url: "https://www.facebook.com/yourprofile",
   },
   {
     Icon: FaTwitter,
@@ -20,6 +21,7 @@ const socialIcons = [
     bgColor: "bg-blue-400",
     textColor: "text-blue-400",
     hoverBgColor: "hover:bg-blue-400",
+    url: "https://www.twitter.com/yourprofile",
   },
   {
     Icon: FaLinkedinIn,
@@ -27,6 +29,7 @@ const socialIcons = [
     bgColor: "bg-blue-700",
     textColor: "text-blue-700",
     hoverBgColor: "hover:bg-blue-700",
+    url: "https://www.linkedin.com/in/yourprofile",
   },
   {
     Icon: FaInstagram,
@@ -34,21 +37,33 @@ const socialIcons = [
     bgColor: "bg-pink-500",
     textColor: "text-pink-500",
     hoverBgColor: "hover:bg-pink-500",
+    url: "https://www.instagram.com/yourprofile",
+  },
+  {
+    Icon: MdEmail,
+    text: "Gmail",
+    bgColor: "bg-red-600",
+    textColor: "text-red-600",
+    hoverBgColor: "hover:bg-red-600",
+    url: "mailto:mooncosmeticscorporate@gmail.com",
   },
 ];
 
 const SocialIcons = () => {
   return (
     <div className="">
-      <div className="flex ">
+      <div className="flex">
         {socialIcons.map((socialIcon, index) => (
-          <div
+          <a
             key={index}
-            className={`flex h-8 w-8  rounded-full mr-3  justify-center items-center transition-colors duration-300 ${socialIcon.textColor} ${socialIcon.hoverBgColor} hover:text-white`}
+            href={socialIcon.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`flex h-8 w-8 rounded-full mr-3 justify-center items-center transition-colors duration-300 ${socialIcon.textColor} ${socialIcon.hoverBgColor} hover:text-white`}
           >
             <socialIcon.Icon className="cursor-pointer text-[14px]" />
             <p className="hidden">{socialIcon.text}</p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
